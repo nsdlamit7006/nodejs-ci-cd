8152c3772bb629772d686c62e27205bc2e86e43b
nsdlamit7006 <nsdlamit7006@gmail.com>
2025-03-14 11:45:14 +0530

    new changes

--- server.js
+++ server.js
@@ -6,4 +6,9 @@ connectDB();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () => {
+    console.log("Hello...")
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = server;
--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,19 @@
 const request = require('supertest');
 const app = require('../app');
+const server = require('../server');
 
-test('POST /register should create user', async () => {
-  const res = await request(app)
-    .post('/api/users/register')
-    .send({ username: 'test', password: '12345' });
-    
-  expect(res.status).toBe(201);
+afterAll(() => {
+  server.close(); // Close server after test
 });
+
+test(
+  'POST /register should create user',
+  async () => {
+    const res = await request(app)
+      .post('/api/users/register')
+      .send({ username: 'test', password: '12345' });
+      
+    expect(res.status).toBe(201);
+  },
+  10000 // Increased timeout to 10 seconds
+);
