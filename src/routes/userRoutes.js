cffde8569b5d88fe142c29ffedeb6293a7539cd2
nsdlamit7006 <nsdlamit7006@gmail.com>
2025-03-18 14:45:30 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/register', userController.register);
+router.post('/register', userController.register1);
 
 module.exports = router;
